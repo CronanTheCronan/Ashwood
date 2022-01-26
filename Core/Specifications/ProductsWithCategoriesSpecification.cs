2258268177e7c622ebcc8c7a1fb270e813e9c695
Matt Cronan <mattcronan.webdev@gmail.com>
2022-01-26 13:45:41 -0600

    add filtering sort and pagination

--- Core/Specifications/ProductsWithCategoriesSpecification.cs
+++ Core/Specifications/ProductsWithCategoriesSpecification.cs
@@ -6,9 +6,31 @@ namespace Core.Specifications
 {
     public class ProductsWithCategoriesSpecification : BaseSpecification<Product>
     {
-        public ProductsWithCategoriesSpecification()
+        public ProductsWithCategoriesSpecification(ProductSpecParams productParams)
+        : base(x => 
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.CategoryId.HasValue || x.ProductCategoryId == productParams.CategoryId)
+          )
         {
             AddInclude(x => x.ProductCategory);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), productParams.PageSize);
+
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDesc(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithCategoriesSpecification(int id) : base(x => x.Id == id)
