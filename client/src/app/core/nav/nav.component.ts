9903675e2af69ccd97f97396f606b57934a246a7
Matt Cronan <mattcronan.webdev@gmail.com>
2022-02-06 13:34:18 -0600

    added redis, basket, checkout

--- client/src/app/core/nav/nav.component.ts
+++ client/src/app/core/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  basket$: Observable<IBasket>;
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
 
   goToLink(link: string) {
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,10 +12,12 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(private shopService: ShopService, 
               private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService) { 
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) { 
                 this.bcService.set('@productDetails',' ');
               }
 
@@ -30,5 +33,18 @@ export class ProductDetailsComponent implements OnInit {
       },
       error: (err) => console.log(err)
     });
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if(this.quantity > 1)
+      this.quantity--;
   }
 }
